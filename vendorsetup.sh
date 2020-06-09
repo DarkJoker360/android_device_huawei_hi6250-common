ae8d5af90b6ff25f468934dbf1d5bfd44a34a271
DarkDroidDev <darkdroid.dev@gmail.com>
2020-06-09 14:59:55 +0200

    hi6250-common: OFox-ify

---
+++ vendorsetup.sh
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2019-2020 OrangeFox Recovery Project
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# See <http://www.gnu.org/licenses/>.
+#
+# Please maintain this if you use this script or any part of it
+#
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export OF_MAINTAINER=DarkDroidDev
+
+# let's see what are our build VARs
+if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+	export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+	export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+	export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+fi
